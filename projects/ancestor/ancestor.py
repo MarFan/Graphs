ce71ba875b78bb61d0951f01a894e7e14c3a187f
Jonah Aitchison <jonah@pop-os.localdomain>
2020-05-12 16:44:14 -0500

    Ugly Ancestor take 2

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -4,26 +4,11 @@ from util import Queue, Stack
 
 def earliest_ancestor(ancestors, starting_node):
 
-    # graph = {}
-    # for p, c in ancestors:
-    #     print(p, c)
-    #     graph.setdefault(p, []).append(c)
-
-    reversed = {}
+    graph = {}
     for p, c in ancestors:
-        if c not in reversed:
-            reversed[c] = []
-        reversed[c].append(p)
-
-    print(reversed)
-    # reversed = {}
-    # for v in graph:
-    #     # print(v, graph[v])
-    #     for e in graph[v]:
-    #         # print(e)
-    #         if e not in reversed:
-    #             reversed[e] = []
-    #         reversed[e].append(v)
+        if c not in graph:
+            graph[c] = []
+        graph[c].append(p)
 
     s = Stack()
     s.push(starting_node)
@@ -33,13 +18,13 @@ def earliest_ancestor(ancestors, starting_node):
         v = s.pop()
         if v not in visited:            
             visited.append(v)
-            if v not in reversed:
+            if v not in graph:
                 if len(visited) > 1:
                     return v
                 else:
                     return -1
 
-            s.push(min(reversed[v]))
+            s.push(min(graph[v]))
 
     return visited.pop()
 
@@ -48,4 +33,4 @@ if __name__ == '__main__':
     system('clear')
     test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
                       (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-    print(earliest_ancestor(test_ancestors, 2))
+    print(earliest_ancestor(test_ancestors, 6))
