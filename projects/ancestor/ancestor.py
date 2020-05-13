28875f4be13767ff94479cfb85ae2d7f3a7eefbf
Jonah Aitchison <jonah@pop-os.localdomain>
2020-05-13 11:29:17 -0500

    Ancestor using lists

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,20 +1,17 @@
-import sys
-sys.path.insert(0, '../graph')
-from util import Queue, Stack
-
 def earliest_ancestor(ancestors, starting_node):
 
+    # reverse list into a dict
     graph = {}
     for p, c in ancestors:
         if c not in graph:
             graph[c] = []
         graph[c].append(p)
 
-    s = Stack()
-    s.push(starting_node)
+    s = []
+    s.append(starting_node)
     visited = []
     
-    while s.size() > 0:
+    while len(s):
         v = s.pop()
         if v not in visited:            
             visited.append(v)
@@ -24,13 +21,11 @@ def earliest_ancestor(ancestors, starting_node):
                 else:
                     return -1
 
-            s.push(min(graph[v]))
+            s.append(min(graph[v]))
 
     return visited.pop()
 
 if __name__ == '__main__':
-    from os import system
-    system('clear')
     test_ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
                       (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
     print(earliest_ancestor(test_ancestors, 6))
